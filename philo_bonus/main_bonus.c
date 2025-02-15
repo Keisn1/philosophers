68deaf7b1f7efea9d0890d7562d7983f8dae86a4
Kay Freyer <kay.freyer@icloud.com>
2025-02-15 16:52:02 +0100

    trying to solve the bonus

--- philo_bonus/main_bonus.c
+++ philo_bonus/main_bonus.c
@@ -15,13 +15,22 @@
 #include <sys/stat.h>        /* For mode constants */
 #include <semaphore.h>
 
-#define FORK_PILE_SEM "/philo_fork_pile"
+#define FORK_PILE_SEM "/philo_fork_pile_sem"
+#define STDOUT_LOCK_SEM "/philo_stdout_lock_sem"
 
 typedef struct s_shared_data {
 	sem_t *fork_pile;
+	sem_t *stdout_lock;
+	sem_t *dead_lock;
 } t_shared_data;
 
-void philo_routine(t_shared_data shared) {
+void print_fork_msg(t_shared_data shared, t_params params, int philo_num) {
+	sem_wait(shared.stdout_lock);
+	printf("%lld %d has taken a fork\n", get_timestamp() - params.base_time, philo_num);
+	sem_post(shared.stdout_lock);
+}
+
+void philo_routine(t_shared_data shared, int philo_num, t_params params) {
 	bool philo_died;
 	int num_forks = 0;
 
@@ -29,12 +38,14 @@ void philo_routine(t_shared_data shared) {
 	while (!philo_died) {
 		while (num_forks != 2) {
 			sem_wait(shared.fork_pile);
+			print_fork_msg(shared, params, philo_num);
 			sem_wait(shared.fork_pile);
+			print_fork_msg(shared, params, philo_num);
 		}
 
 
-		sem_wait(shared.fork_pile);
-		sem_wait(shared.fork_pile);
+		sem_post(shared.fork_pile);
+		sem_post(shared.fork_pile);
 	}
 }
 
@@ -47,15 +58,29 @@ int	main(int argc, char **argv)
 	params = get_params(argc, argv);
 
 
-	shared.fork_pile = sem_open(FORK_PILE_SEM, O_CREAT, 0644, 1);
+	shared.fork_pile = sem_open(FORK_PILE_SEM, O_CREAT, 0644, params.num_philos);
 	if (shared.fork_pile == SEM_FAILED) {
+		perror("sem_open failed");
+		exit(EXIT_FAILURE);
+	}
+	shared.stdout_lock = sem_open(STDOUT_LOCK_SEM, O_CREAT, 0644, params.num_philos);
+	if (shared.stdout_lock == SEM_FAILED) {
+		perror("sem_open failed");
+		exit(EXIT_FAILURE);
+	}
+	shared.dead_lock = sem_open(STDOUT_LOCK_SEM, O_CREAT, 0644, params.num_philos);
+	if (shared.dead_lock == SEM_FAILED) {
 		perror("sem_open failed");
 		exit(EXIT_FAILURE);
 	}
 
 
+	sem_wait(shared.dead_lock);
+
+
 	sem_close(shared.fork_pile);
 	sem_unlink(FORK_PILE_SEM);
-
+	sem_close(shared.stdout_lock);
+	sem_unlink(FORK_PILE_SEM);
 	return (0);
 }
